557457272c3ede6dee6f8fe6f39cd19b76c5856e
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-14 10:44:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+// array of band names
+let bandNames = ['The Rolling Stones', 'Pink Floyd', 'Led Zeppelin', 'The Beatles', 'AC/DC'];
+
+// function to remove articles from band names
+function removeArticles(name) {
+  // regular expression to match articles at the beginning of a string
+  const regex = /^(?:the|an|a)\s+/i;
+  // replace article with an empty string
+  return name.replace(regex, '');
+}
+
+// sort band names in lexicographic order excluding articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// get ul element by id
+const ul = document.getElementById('band');
+
+// create list item for each band name and append to ul
+bandNames.forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
+
 
