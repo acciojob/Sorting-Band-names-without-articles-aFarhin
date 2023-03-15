8bbdc100b738b9061190cc2a2f99abaedb8eef0e
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-15 11:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-//your code here
-// array of band names
-let bandNames = ['The Rolling Stones', 'Pink Floyd', 'Led Zeppelin', 'The Beatles', 'AC/DC'];
+// Input array of band names
+let bandNames = ["The Beatles", "Rolling Stones", "Led Zeppelin", "Pink Floyd", "The Who", "The Doors"];
 
-// function to remove articles from band names
-function removeArticles(name) {
-  // regular expression to match articles at the beginning of a string
-  const regex = /^(?:the|an|a)\s+/i;
-  // replace article with an empty string
-  return name.replace(regex, '');
+// Function to remove articles from band names
+function removeArticle(bandName) {
+  // List of articles to remove
+  let articles = ["a", "an", "the"];
+  // Split band name into words
+  let words = bandName.split(" ");
+  // Check if first word is an article
+  if (articles.includes(words[0].toLowerCase())) {
+    // Remove the article
+    words.shift();
+  }
+  // Join the remaining words and return the modified band name
+  return words.join(" ");
 }
 
-// sort band names in lexicographic order excluding articles
-bandNames.sort();
-
-// get ul element by id
-const ul = document.getElementById('band');
-
-// create list item for each band name and append to ul
-bandNames.forEach(name => {
-  const li = document.createElement('li');
-  li.textContent = name;
-  ul.appendChild(li);
+// Sort the band names in lexicographic order excluding articles
+bandNames.sort(function(a, b) {
+  return removeArticle(a).localeCompare(removeArticle(b));
 });
 
+// Get the ul element with id 'band'
+let ulElement = document.getElementById("band");
 
+// Loop through the sorted band names and add them as li elements to the ul element
+for (let i = 0; i < bandNames.length; i++) {
+  let liElement = document.createElement("li");
+  liElement.textContent = bandNames[i];
+  ulElement.appendChild(liElement);
+}
